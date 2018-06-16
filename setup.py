7c396d3bc91a99b0c66c542e13a272dc97002cd3
Alex Nichol <unixpickle@gmail.com>
2018-06-16 15:12:57 -0700

    add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='tf-env',
+    version='0.0.1',
+    description='RL environments for TensorFlow.',
+    url='https://github.com/unixpickle/tf-env',
+    author='Alex Nichol',
+    author_email='unixpickle@gmail.com',
+    packages=['tf_env'],
+    install_requires=[
+        'numpy',
+        'anyrl>=0.11.0,<0.13.0'
+    ],
+    extras_require={
+        "tf": ["tensorflow>=1.4.0"],
+        "tf_gpu": ["tensorflow-gpu>=1.4.0"]
+    }
+)
