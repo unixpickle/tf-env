d22cb2840cc62c4cf9fcfa91b455681139801048
Alex Nichol <unixpickle@gmail.com>
2018-06-25 17:35:38 -0700

    factor out common random agent code

--- example/point_seeker_random.py
+++ example/point_seeker_random.py
@@ -2,32 +2,14 @@
 Watch random agent play PointSeeker.
 """
 
-import time
-
-from gym.envs.classic_control.rendering import SimpleImageViewer
-import tensorflow as tf
-
 from tf_env import PointSeeker
+
+from watch_random import watch_random
 
 
 def main():
     env = PointSeeker()
-    init_state = env.reset(1)
-    states = tf.placeholder(init_state.dtype, shape=init_state.get_shape())
-    actions = tf.random_uniform(shape=[1], minval=0, maxval=env.num_actions, dtype=tf.int32)
-    new_states, rews, dones = env.step(states, actions)
-    image = env.observe_visual(states)
-    viewer = SimpleImageViewer()
-    with tf.Session() as sess:
-        cur_states = sess.run(init_state)
-        while True:
-            cur_states, cur_rews, cur_dones = sess.run([new_states, rews, dones],
-                                                       feed_dict={states: cur_states})
-            cur_image = sess.run(image, feed_dict={states: cur_states})
-            viewer.imshow(cur_image[0])
-            if cur_dones[0]:
-                print('done with reward: %f' % cur_rews[0])
-            time.sleep(1.0 / 10)
+    watch_random(env, observe_fn=env.observe_visual, frame_rate=20.0)
 
 
 if __name__ == '__main__':
